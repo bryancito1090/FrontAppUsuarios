eb1b87c6a8b15460f951f673126bf73e14a978ae
havicDevs <139989428+bryancito1090@users.noreply.github.com>
2024-07-02 23:24:39 -0500

    add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component'; // Asegúrate de ajustar la ruta de importación según la estructura de tu proyecto
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent }
+];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  // Variables y métodos del componente van aquí
+  constructor() { }
+
+  ngOnInit(): void {
+    // Lógica de inicialización del componente
+  }
+}
